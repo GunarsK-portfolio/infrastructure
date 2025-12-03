0c0d0af7d4177e52d811f814b433fa02bc11352f
GunarsK <gunarskunakovs@gmail.com>
2025-12-03 12:14:01 +0200

    Fix rabbit host url - port is added in Go services

--- terraform/modules/mq/outputs.tf
+++ terraform/modules/mq/outputs.tf
@@ -11,9 +11,16 @@ output "broker_arn" {
 }
 
 output "amqp_endpoint" {
-  description = "AMQP endpoint for RabbitMQ connections"
+  description = "AMQP endpoint for RabbitMQ connections (full URL)"
   value       = aws_mq_broker.main.instances[0].endpoints[0]
   sensitive   = true
+}
+
+output "amqp_host" {
+  description = "AMQP hostname for RabbitMQ connections (without protocol/port)"
+  # Extract hostname from amqps://b-xxx.mq.region.on.aws:5671
+  value     = regex("amqps://([^:]+):", aws_mq_broker.main.instances[0].endpoints[0])[0]
+  sensitive = true
 }
 
 output "console_url" {
